38e2b72fd694344ceb28891380bcfa40a44f15e4
Zeynep Uzun <zeynepuz2003@gmail.com>
2026-03-09 23:18:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import json
 email = "EMAILINIZ"
 api_token = "TOKENINIZ"
 domain = "SIRKETADINIZ.atlassian.net"
+email = "zeynepuz2003@gmail.com"
+api_token = "ATATT3xFfGF0_rXI42h92VOMVsKnpTWRmVjkqzi8GFUf5qi7Z2E_RgkWkXs2LvbIpO82JJc7VUltJdQKZZWiDSmvQh2uT6EZwAVZ6clxZUM9F9R-k9i0vRi7njrAQntwPYpKm_OCxpBxteYe9NDKZVz_Z8AgQ86DDBPQx4Vg6n9n4-V9gCLeOCk=D3FAC25A"
+domain = "zeynepuz200345.atlassian.net"
 
 url = f"https://{domain}/rest/api/3/search"
+url = f"https://{domain}/rest/api/3/search/jql"
 
 auth = HTTPBasicAuth(email, api_token)
 
@@ -16,6 +20,7 @@ headers = {
 
 query = {
    'jql': 'project = PROJEKODU'
+   'jql': 'project = DEV'
 }
 
 response = requests.get(
@@ -27,5 +32,7 @@ response = requests.get(
 
 data = response.json()
 
+print(data)
+
 for issue in data["issues"]:
-    print(issue["key"], issue["fields"]["summary"])
+    print(issue["key"], issue["fields"]["summary"])    print(issue)
