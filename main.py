f1f5033468d79c57702d39b646ede4ee9b2732b3
Selininan <oselin.inan@gmail.com>
2026-03-09 18:36:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Proje çalışıyor")
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+email = "EMAILINIZ"
+api_token = "TOKENINIZ"
+domain = "SIRKETADINIZ.atlassian.net"
+
+url = f"https://{domain}/rest/api/3/search"
+
+auth = HTTPBasicAuth(email, api_token)
+
+headers = {
+  "Accept": "application/json"
+}
+
+query = {
+   'jql': 'project = PROJEKODU'
+}
+
+response = requests.get(
+   url,
+   headers=headers,
+   params=query,
+   auth=auth
+)
+
+data = response.json()
+
+for issue in data["issues"]:
+    print(issue["key"], issue["fields"]["summary"])
